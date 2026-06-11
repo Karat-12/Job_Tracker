52c1f896bdd449c4f085b0239ed2e058c8e280aa
Karat-12 <karthikagrawal05@gmail.com>
2026-06-11 12:29:47 +0530

    Test direct mongo uri t2

--- backend/demo/src/main/java/com/jobtracker/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/jobtracker/demo/DemoApplication.java
@@ -1,19 +1,26 @@
 package com.jobtracker.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		    System.out.println("========== DEBUG ==========");
-    System.out.println("MONGODB_URI = " + System.getenv("MONGODB_URI"));
-    System.out.println("===========================");
+    @Autowired
+    private Environment env;
 
-    SpringApplication.run(DemoApplication.class, args);
+    @PostConstruct
+    public void debug() {
+        System.out.println("================================");
+        System.out.println("ENV MONGODB_URI = " + System.getenv("MONGODB_URI"));
+        System.out.println("SPRING URI = " + env.getProperty("spring.data.mongodb.uri"));
+        System.out.println("================================");
+    }
 
-   
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
