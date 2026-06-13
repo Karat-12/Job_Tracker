db99aec1e0075726d7273667f509b7ab8bbefd33
Karat-12 <karthikagrawal05@gmail.com>
2026-06-13 22:45:00 +0530

    fix mongodb config

--- backend/demo/src/main/java/com/jobtracker/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/jobtracker/demo/DemoApplication.java
@@ -1,24 +1,10 @@
 package com.jobtracker.demo;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @Autowired
-    private Environment env;
-
-    @PostConstruct
-    public void debug() {
-        System.out.println("================================");
-        System.out.println("ENV MONGODB_URI = " + System.getenv("MONGODB_URI"));
-        System.out.println("SPRING URI = " + env.getProperty("spring.data.mongodb.uri"));
-        System.out.println("================================");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
